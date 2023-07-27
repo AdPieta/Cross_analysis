a8b1e2301ddf6d46f3892aa64e007d5fd2811bc0
AdPieta <76159634+AdPieta@users.noreply.github.com>
2023-07-27 19:38:26 +0200

    Add files via upload

---
+++ Opta_team_dict.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 11 21:07:44 2023
+
+@author: adamm
+"""
+
+opta_team_dict = {'1sxaf8l7fknmucd72fdretogm':'Rakow Czestochowa',
+                  '2672w7jn6onmk0lmpsp94eutg':'Slask Wroclaw',
+                  '13tihpiruaaa1xreofk5ovk43':'Lechia Gdansk',
+                  'duvz7klp081jpg8fx5nze7ks7':'Lech Poznan',
+                  'dr2kq4jv0qx4qtui51vrkft6k':'Miedz Legnica',
+                  'd7udukc1t631drge83p7ucqws':'Legia Warszawa',
+                  '3ail506t69cxr60r1p6wlfqmb':'Stal Mielec',
+                  '7r1d7xdgcjgp61qxshpho7xck':'Pogon Szczecin',
+                  '145oko43h2g50ck9kn3d51xae':'Zaglebie Lubin',
+                  'awc8tinkh5u7cy58a00ohg6al':'Gornik Zabrze',
+                  '2l1pdpvptdw9ois9qkrs9xi3c':'Korona Kielce',
+                  '8jw3s68b9lil5xcgjvbcmrgc2':'Jagiellonia',
+                  'cbq36zdxvn152966h49hb4ijm':'Cracovia',
+                  'afx3euq4owjdonw2gmsu842nz':'Wisla Plock',
+                  'd7i1aj8vnexyqv76yt03d54wc':'Piast Gliwice',
+                  '40hitpa3iyfvhdx40fj3cltd':'Widzew Lodz',
+                  '5iq037iifttb7jw2zkz2z57pj':'Radomiak Radom',
+                  '3sw1izq8c4reenf6egfktveh1':'Warta Poznan'}
